8ef5a9aa93431b08866ccf683b890166973a0e29
erysdren <contact@erysdren.me>
2025-09-06 00:19:08 -0500

    refactor CMakeLists.txt and add SDL3 bridge+examples

---
+++ examples/harness_sdl3.c
@@ -0,0 +1,180 @@
+/*
+MIT License
+
+Copyright (c) 2023-2025 erysdren (it/its)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+ *
+ * HARNESS_SDL3.C
+ *
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <SDL3/SDL.h>
+
+#include "eui_sdl3.h"
+#include "examples.h"
+
+#include "palette_vga.h"
+
+/* SDL2 state */
+static SDL_Window *window;
+static SDL_Surface *surface8;
+static SDL_Surface *surface32;
+static SDL_Renderer *renderer;
+static SDL_Texture *texture;
+static SDL_Rect rect;
+static SDL_Color colors[256];
+static SDL_Event event;
+
+#ifdef EXAMPLE_STANDALONE
+#define MOUSE_GRAB_PADDING (10)
+SDL_HitTestResult hit_test(SDL_Window *window, const SDL_Point *point, void *user)
+{
+	EUI_UNUSED(window);
+	EUI_UNUSED(user);
+
+	if (point->y <= 16)
+		return SDL_HITTEST_DRAGGABLE;
+	else
+		return SDL_HITTEST_NORMAL;
+}
+#endif
+
+/* main */
+int main(int argc, char **argv)
+{
+	uint32_t format;
+	int i;
+
+	EUI_UNUSED(argc);
+	EUI_UNUSED(argv);
+
+	/* init */
+	SDL_Init(SDL_INIT_VIDEO);
+
+	/* create window */
+	window = SDL_CreateWindow(EXAMPLE_TITLE,
+		EXAMPLE_WIDTH, EXAMPLE_HEIGHT,
+		SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIDDEN
+	);
+
+	/* create renderer */
+	renderer = SDL_CreateRenderer(window, NULL);
+	SDL_SetRenderVSync(renderer, 1);
+	SDL_SetRenderLogicalPresentation(renderer, EXAMPLE_WIDTH, EXAMPLE_HEIGHT, SDL_LOGICAL_PRESENTATION_INTEGER_SCALE);
+	SDL_SetWindowMinimumSize(window, EXAMPLE_WIDTH, EXAMPLE_HEIGHT);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+#ifdef EXAMPLE_STANDALONE
+	SDL_SetWindowBordered(window, false);
+	SDL_SetWindowHitTest(window, hit_test, 0);
+#endif
+
+	/* create our render surface */
+	surface8 = SDL_CreateSurface(EXAMPLE_WIDTH, EXAMPLE_HEIGHT, SDL_PIXELFORMAT_INDEX8);
+	SDL_FillSurfaceRect(surface8, NULL, 0);
+
+	/* init palette */
+	for (i = 0; i < 256; i++)
+	{
+		colors[i].r = palette_vga[i * 3];
+		colors[i].g = palette_vga[i * 3 + 1];
+		colors[i].b = palette_vga[i * 3 + 2];
+		colors[i].a = SDL_ALPHA_OPAQUE;
+	}
+
+	/* install palette */
+	SDL_SetPaletteColors(SDL_CreateSurfacePalette(surface8), colors, 0, 256);
+
+	/* create display surface */
+	format = SDL_GetWindowPixelFormat(window);
+	surface32 = SDL_CreateSurface(EXAMPLE_WIDTH, EXAMPLE_HEIGHT, format);
+	texture = SDL_CreateTexture(renderer, format, SDL_TEXTUREACCESS_STREAMING, EXAMPLE_WIDTH, EXAMPLE_HEIGHT);
+	SDL_SetTextureScaleMode(texture, SDL_SCALEMODE_NEAREST);
+
+	/* make sure relative mouse mode is disabled */
+	SDL_SetWindowRelativeMouseMode(window, false);
+
+	/* setup blit rect */
+	rect.x = 0;
+	rect.y = 0;
+	rect.w = EXAMPLE_WIDTH;
+	rect.h = EXAMPLE_HEIGHT;
+
+	/* init eui */
+	eui_init(surface8->w, surface8->h, SDL_BITSPERPIXEL(surface8->format), surface8->pitch, surface8->pixels);
+
+	SDL_ShowWindow(window);
+
+	/* main loop */
+	while (true)
+	{
+		/* push events */
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_EVENT_QUIT)
+				goto done;
+			eui_sdl3_event_push(&event);
+		}
+
+		/* process events */
+		eui_event_queue_process();
+
+		/* clear screen */
+		SDL_FillSurfaceRect(surface8, NULL, 0x00);
+
+		/* run eui context */
+		if (eui_context_begin())
+		{
+			/* run example func */
+			EXAMPLE_FUNC();
+
+			/* end eui context */
+			eui_context_end();
+		}
+
+		/* copy to screen */
+		SDL_BlitSurface(surface8, &rect, surface32, &rect);
+		SDL_UpdateTexture(texture, NULL, surface32->pixels, surface32->pitch);
+		SDL_RenderClear(renderer);
+		SDL_RenderTexture(renderer, texture, NULL, NULL);
+		SDL_RenderPresent(renderer);
+	}
+
+done:
+	/* quit */
+	eui_quit();
+	SDL_DestroySurface(surface8);
+	SDL_DestroySurface(surface32);
+	SDL_DestroyTexture(texture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
---
+++ source/eui_sdl3.c
@@ -0,0 +1,234 @@
+/*
+MIT License
+
+Copyright (c) 2023-2025 erysdren (it/its)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+ *
+ * EUI_SDL3.C
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#include "eui_sdl3.h"
+
+/*
+ *
+ * state
+ *
+ */
+
+static int scancode_table_initialized = EUI_FALSE;
+static int scancode_table[SDL_SCANCODE_COUNT];
+
+/*
+ *
+ * private functions
+ *
+ */
+
+static void initialize_scancode_table(void)
+{
+	if (scancode_table_initialized)
+		return;
+
+	scancode_table[SDL_SCANCODE_ESCAPE] = EUI_SCANCODE_ESCAPE;
+	scancode_table[SDL_SCANCODE_1] = EUI_SCANCODE_1;
+	scancode_table[SDL_SCANCODE_2] = EUI_SCANCODE_2;
+	scancode_table[SDL_SCANCODE_3] = EUI_SCANCODE_3;
+	scancode_table[SDL_SCANCODE_4] = EUI_SCANCODE_4;
+	scancode_table[SDL_SCANCODE_5] = EUI_SCANCODE_5;
+	scancode_table[SDL_SCANCODE_6] = EUI_SCANCODE_6;
+	scancode_table[SDL_SCANCODE_7] = EUI_SCANCODE_7;
+	scancode_table[SDL_SCANCODE_8] = EUI_SCANCODE_8;
+	scancode_table[SDL_SCANCODE_9] = EUI_SCANCODE_9;
+	scancode_table[SDL_SCANCODE_0] = EUI_SCANCODE_0;
+	scancode_table[SDL_SCANCODE_MINUS] = EUI_SCANCODE_MINUS;
+	scancode_table[SDL_SCANCODE_EQUALS] = EUI_SCANCODE_EQUALS;
+	scancode_table[SDL_SCANCODE_BACKSPACE] = EUI_SCANCODE_BACKSPACE;
+	scancode_table[SDL_SCANCODE_TAB] = EUI_SCANCODE_TAB;
+	scancode_table[SDL_SCANCODE_Q] = EUI_SCANCODE_Q;
+	scancode_table[SDL_SCANCODE_W] = EUI_SCANCODE_W;
+	scancode_table[SDL_SCANCODE_E] = EUI_SCANCODE_E;
+	scancode_table[SDL_SCANCODE_R] = EUI_SCANCODE_R;
+	scancode_table[SDL_SCANCODE_T] = EUI_SCANCODE_T;
+	scancode_table[SDL_SCANCODE_Y] = EUI_SCANCODE_Y;
+	scancode_table[SDL_SCANCODE_U] = EUI_SCANCODE_U;
+	scancode_table[SDL_SCANCODE_I] = EUI_SCANCODE_I;
+	scancode_table[SDL_SCANCODE_O] = EUI_SCANCODE_O;
+	scancode_table[SDL_SCANCODE_P] = EUI_SCANCODE_P;
+	scancode_table[SDL_SCANCODE_LEFTBRACKET] = EUI_SCANCODE_LEFTBRACKET;
+	scancode_table[SDL_SCANCODE_RIGHTBRACKET] = EUI_SCANCODE_RIGHTBRACKET;
+	scancode_table[SDL_SCANCODE_RETURN] = EUI_SCANCODE_ENTER;
+	scancode_table[SDL_SCANCODE_LCTRL] = EUI_SCANCODE_CONTROL;
+	scancode_table[SDL_SCANCODE_RCTRL] = EUI_SCANCODE_CONTROL;
+	scancode_table[SDL_SCANCODE_A] = EUI_SCANCODE_A;
+	scancode_table[SDL_SCANCODE_S] = EUI_SCANCODE_S;
+	scancode_table[SDL_SCANCODE_D] = EUI_SCANCODE_D;
+	scancode_table[SDL_SCANCODE_F] = EUI_SCANCODE_F;
+	scancode_table[SDL_SCANCODE_G] = EUI_SCANCODE_G;
+	scancode_table[SDL_SCANCODE_H] = EUI_SCANCODE_H;
+	scancode_table[SDL_SCANCODE_J] = EUI_SCANCODE_J;
+	scancode_table[SDL_SCANCODE_K] = EUI_SCANCODE_K;
+	scancode_table[SDL_SCANCODE_L] = EUI_SCANCODE_L;
+	scancode_table[SDL_SCANCODE_SEMICOLON] = EUI_SCANCODE_SEMICOLON;
+	scancode_table[SDL_SCANCODE_APOSTROPHE] = EUI_SCANCODE_QUOTE;
+	scancode_table[SDL_SCANCODE_GRAVE] = EUI_SCANCODE_TILDE;
+	scancode_table[SDL_SCANCODE_LSHIFT] = EUI_SCANCODE_LSHIFT;
+	scancode_table[SDL_SCANCODE_BACKSLASH] = EUI_SCANCODE_BACKSLASH;
+	scancode_table[SDL_SCANCODE_Z] = EUI_SCANCODE_Z;
+	scancode_table[SDL_SCANCODE_X] = EUI_SCANCODE_X;
+	scancode_table[SDL_SCANCODE_C] = EUI_SCANCODE_C;
+	scancode_table[SDL_SCANCODE_V] = EUI_SCANCODE_V;
+	scancode_table[SDL_SCANCODE_B] = EUI_SCANCODE_B;
+	scancode_table[SDL_SCANCODE_N] = EUI_SCANCODE_N;
+	scancode_table[SDL_SCANCODE_M] = EUI_SCANCODE_M;
+	scancode_table[SDL_SCANCODE_COMMA] = EUI_SCANCODE_COMMA;
+	scancode_table[SDL_SCANCODE_PERIOD] = EUI_SCANCODE_PERIOD;
+	scancode_table[SDL_SCANCODE_SLASH] = EUI_SCANCODE_SLASH;
+	scancode_table[SDL_SCANCODE_RSHIFT] = EUI_SCANCODE_RSHIFT;
+	scancode_table[SDL_SCANCODE_KP_MULTIPLY] = EUI_SCANCODE_MULTIPLY;
+	scancode_table[SDL_SCANCODE_LALT] = EUI_SCANCODE_ALT;
+	scancode_table[SDL_SCANCODE_SPACE] = EUI_SCANCODE_SPACE;
+	scancode_table[SDL_SCANCODE_CAPSLOCK] = EUI_SCANCODE_CAPSLOCK;
+	scancode_table[SDL_SCANCODE_F1] = EUI_SCANCODE_F1;
+	scancode_table[SDL_SCANCODE_F2] = EUI_SCANCODE_F2;
+	scancode_table[SDL_SCANCODE_F3] = EUI_SCANCODE_F3;
+	scancode_table[SDL_SCANCODE_F4] = EUI_SCANCODE_F4;
+	scancode_table[SDL_SCANCODE_F5] = EUI_SCANCODE_F5;
+	scancode_table[SDL_SCANCODE_F6] = EUI_SCANCODE_F6;
+	scancode_table[SDL_SCANCODE_F7] = EUI_SCANCODE_F7;
+	scancode_table[SDL_SCANCODE_F8] = EUI_SCANCODE_F8;
+	scancode_table[SDL_SCANCODE_F9] = EUI_SCANCODE_F9;
+	scancode_table[SDL_SCANCODE_F10] = EUI_SCANCODE_F10;
+	scancode_table[SDL_SCANCODE_NUMLOCKCLEAR] = EUI_SCANCODE_NUMLOCK;
+	scancode_table[SDL_SCANCODE_SCROLLLOCK] = EUI_SCANCODE_SCROLLLOCK;
+	scancode_table[SDL_SCANCODE_HOME] = EUI_SCANCODE_HOME;
+	scancode_table[SDL_SCANCODE_UP] = EUI_SCANCODE_UP;
+	scancode_table[SDL_SCANCODE_PAGEUP] = EUI_SCANCODE_PAGEUP;
+	scancode_table[SDL_SCANCODE_LEFT] = EUI_SCANCODE_LEFT;
+	scancode_table[SDL_SCANCODE_RIGHT] = EUI_SCANCODE_RIGHT;
+	scancode_table[SDL_SCANCODE_KP_PLUS] = EUI_SCANCODE_PLUS;
+	scancode_table[SDL_SCANCODE_END] = EUI_SCANCODE_END;
+	scancode_table[SDL_SCANCODE_DOWN] = EUI_SCANCODE_DOWN;
+	scancode_table[SDL_SCANCODE_PAGEDOWN] = EUI_SCANCODE_PAGEDOWN;
+	scancode_table[SDL_SCANCODE_INSERT] = EUI_SCANCODE_INSERT;
+	scancode_table[SDL_SCANCODE_DELETE] = EUI_SCANCODE_DELETE;
+	scancode_table[SDL_SCANCODE_F11] = EUI_SCANCODE_F11;
+	scancode_table[SDL_SCANCODE_F12] = EUI_SCANCODE_F12;
+
+	scancode_table_initialized = EUI_TRUE;
+}
+
+/*
+ *
+ * public functions
+ *
+ */
+
+/* process and push SDL_Event */
+void eui_sdl3_event_push(SDL_Event *event)
+{
+	eui_event_t eui_event;
+
+	initialize_scancode_table();
+
+	switch (event->type)
+	{
+		case SDL_EVENT_KEY_DOWN:
+			if (scancode_table[event->key.scancode])
+			{
+				eui_event.type = EUI_EVENT_KEY_DOWN;
+				eui_event.key.scancode = scancode_table[event->key.scancode];
+				eui_event_push(&eui_event);
+			}
+			break;
+
+		case SDL_EVENT_KEY_UP:
+			if (scancode_table[event->key.scancode])
+			{
+				eui_event.type = EUI_EVENT_KEY_UP;
+				eui_event.key.scancode = scancode_table[event->key.scancode];
+				eui_event_push(&eui_event);
+			}
+			break;
+
+		case SDL_EVENT_MOUSE_BUTTON_DOWN:
+			switch (event->button.button)
+			{
+				case SDL_BUTTON_LEFT:
+					eui_event.type = EUI_EVENT_BUTTON_DOWN;
+					eui_event.button.x = event->button.x;
+					eui_event.button.y = event->button.y;
+					eui_event.button.button = EUI_BUTTON_LEFT;
+					eui_event_push(&eui_event);
+					break;
+
+				case SDL_BUTTON_RIGHT:
+					eui_event.type = EUI_EVENT_BUTTON_DOWN;
+					eui_event.button.x = event->button.x;
+					eui_event.button.y = event->button.y;
+					eui_event.button.button = EUI_BUTTON_RIGHT;
+					eui_event_push(&eui_event);
+					break;
+			}
+			break;
+
+		case SDL_EVENT_MOUSE_BUTTON_UP:
+			switch (event->button.button)
+			{
+				case SDL_BUTTON_LEFT:
+					eui_event.type = EUI_EVENT_BUTTON_UP;
+					eui_event.button.x = event->button.x;
+					eui_event.button.y = event->button.y;
+					eui_event.button.button = EUI_BUTTON_LEFT;
+					eui_event_push(&eui_event);
+					break;
+
+				case SDL_BUTTON_RIGHT:
+					eui_event.type = EUI_EVENT_BUTTON_UP;
+					eui_event.button.x = event->button.x;
+					eui_event.button.y = event->button.y;
+					eui_event.button.button = EUI_BUTTON_RIGHT;
+					eui_event_push(&eui_event);
+					break;
+			}
+			break;
+
+		case SDL_EVENT_MOUSE_MOTION:
+			eui_event.type = EUI_EVENT_CURSOR;
+			eui_event.cursor.x = event->motion.x;
+			eui_event.cursor.y = event->motion.y;
+			eui_event.cursor.xrel = event->motion.xrel;
+			eui_event.cursor.yrel = event->motion.yrel;
+			eui_event_push(&eui_event);
+			break;
+
+		default:
+			break;
+	}
+}
